0ad669bcc6f192fe5f745b9c4367504db2172b5c
k0hei2025 <vaibhavvermaonline@gmail.com>
2021-07-20 22:18:27 +0530

    done

--- src/Component/home.jsx
+++ src/Component/home.jsx
@@ -99,6 +99,181 @@ export default function home() {
                               <Grid item md={3} sm={0}></Grid>
                                 
                              </Grid>
+              
+              <Grid  container spacing={3} className={classes.grid2}>
+
+              <Grid item md={12} sm={12}>
+              <Grid container>
+                <Grid item md={4}></Grid>
+ 
+    <Grid item md={4}>
+
+                <h1>Our Product</h1>
+
+                <p> Dezy It, is your personal design sprint master. It is your design thinking guide and companion to help you facilitate design sprints, to innovate, pivot and improve on the user experience of your product.</p>
+                </Grid>
+                 <Grid item md={4}></Grid>
+            </Grid>
+              </Grid>
+
+
+
+              <Grid item md={12} sm={12}>
+               
+
+               <Grid container spacing={3} >
+
+<Grid item md={1}> </Grid>
+<Grid item md={2}> </Grid>
+
+               <Grid item md={2} sm={2}>
+
+           <img src="https://static.wixstatic.com/media/ec9816_84599e042dc34d3a9b819b453f67aad5~mv2.png/v1/fill/w_328,h_397,al_c,q_95/Design%2520Thinking%2520Sprint%2520-Right4_edi.webp"/>
+
+               </Grid>
+               <Grid item md={2} sm={2}>
+     
+               <img src="https://static.wixstatic.com/media/ec9816_6b99a07b335a4c11822c71ab4a70a6ea~mv2.png/v1/fill/w_280,h_450,al_c,q_95/DesignSprint-Prototype.webp"/>
+               
+             
+
+               </Grid>
+
+<Grid item md={3} sm={2}>
+                 <img src="https://static.wixstatic.com/media/ec9816_3ebb0016a042454381fa1b369f3b0aeb~mv2.png/v1/fill/w_328,h_397,al_c,q_95/Design%20Thinking%20Sprint%20-%20Left1_edited_pn.webp"/>
+</Grid>
+
+              </Grid>
+              </Grid>
+
+              <Grid item md={12} sm={12} >
+
+<Grid container spacing={3}>
+
+               <Grid item md={5}></Grid>
+               <Grid item md={4}>
+
+               <h3>Download Now!</h3>
+             <b>  And enjoy our Beta Version for free! </b> <br/>
+             <br />
+                <img  style={{
+                                 width:'300px',
+                                 height:'40px'
+                              }} src="https://www.freepnglogos.com/uploads/app-store-logo-png/google-play-and-apple-app-store-logos-22.png"/>
+              </Grid>
+              </Grid>
+               <Grid item md={4}></Grid>
+              </Grid>
+
+              </Grid> 
+
+
+
+
+
+        <Grid container style={{
+                       backgroundColor:'white'
+        }}>
+   <Grid item md="12"  >
+   <Grid container >
+   <Grid item md={5}></Grid>
+    <Grid item md={4}>
+                      <h1>Why Dezy It?</h1>
+    </Grid>
+     <Grid item md={4}></Grid>
+
+   </Grid>
+</Grid>
+
+<Grid item md="6">
+<Grid container>
+<Grid item md={6}></Grid>
+       
+       <Grid item md={4} sm={12}>
+        <h1>Collabaration </h1>
+        <p>The product development journey can become a linear process where there is very minimal interaction between the product team and all the other stakeholders, including the marketers, the sales team and customer service. Dezy It, brings in a collaborative space for your team to leverage design thinking and work together and participate in the ideation process, sharing valuable insights which would shape the product and its features. It's a process which truly gets your whole team invested!</p>
+        </Grid>
+</Grid>
+ </Grid>
+
+ <Grid item md="6" sm={12}>
+          <img src="https://static.wixstatic.com/media/ec9816_d3949d97e43843b8a292ffe04eacce85~mv2.png/v1/fill/w_600,h_450,al_c,q_95/Design%20Sprint%20-%20Design%20Thinking%20-%20Graphi.webp" width="350px" height="350px"/>
+ </Grid>
+
+        </Grid>
+
+      
+  <Grid container style={{
+                       backgroundColor:'white'
+        }}>
+   
+
+
+
+<Grid item md="6">
+<Grid container>
+
+
+
+<Grid item md={5}></Grid>
+       
+       <Grid item md={4} sm={12}>
+        <img src="https://static.wixstatic.com/media/ec9816_02665f5bd8044684b66a011b3c3bd8ca~mv2.png/v1/fill/w_600,h_450,al_c,q_95/Design%20Sprint%20-%20Design%20Thinking%20-%20Graphi.webp" width="350px" height="350px"/>
+       
+        </Grid>
+</Grid>
+ </Grid>
+       
+ <Grid item md="6">
+       <Grid container>
+
+
+ <Grid item md={4} sm={12}>
+        <h1>
+Creativity </h1>
+        <p>Design Sprints are a great way to quickly innovate. A structured agile process to streamline your innovation and come up with multiple creative ideas using the Crazy-8 framework. It boosts your morale and provokes you to think out of the box. Now, with a ton of ideas, the Impact vs Feasibility analysis is a simple yet effective criteria, to shortlist the winning ides and most importantly validate them with real users</p>
+        </Grid>
+        <Grid item md={4}></Grid>
+         
+ </Grid>
+</Grid>
+
+        </Grid>
+
+  <Grid container style={{
+                       backgroundColor:'white'
+        }}>
+  
+
+<Grid item md="6">
+<Grid container>
+<Grid item md={6}></Grid>
+       
+       <Grid item md={4} sm={12}>
+        <h1>Empathy
+ </h1>
+        <p>The first rule of design thinking is to keep your users in the centre and under a spotlight.
+
+All great ideas arrive from devastating problems. The design sprint focuses on getting each and every team member to be able to empathise and understand the user, map their journeys and identify their pain points. You have to truly step into the shoes of your users to understand their psychology and experience of what they feel and think; while using your product or service.</p>
+        </Grid>
+</Grid>
+ </Grid>
+
+ <Grid item md="6" sm={12}>
+          <img src="https://static.wixstatic.com/media/ec9816_2f2c8bead30544a89198bb3d605d232a~mv2.png/v1/fill/w_600,h_450,al_c,q_95/Design%20Sprint%20-%20Design%20Thinking%20-%20Graphi.webp" width="350px" height="350px"/>
+ </Grid>
+
+        </Grid>
+
+
+
+
+
+
+
+
+
+
 
 </Fragment>
                
